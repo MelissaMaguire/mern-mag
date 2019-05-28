b55bc640d0a93491fc6b645f8e532c72e52b1cee
melissa maguire <melissapaigemaguire98@gmail.com>
2019-05-27 23:30:16 -0700

    yay

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mern-mag', {
     useNewUrlParser: true,
     useCreateIndex: true
-})
+}) 
